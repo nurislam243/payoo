816048ba8fa991e3c91b2dfaf3d64aac3844fa7a
nurislam243 <mdnurislam@gmail.com>
2025-02-25 19:36:02 +0600

    added login form

---
+++ script/login.js
@@ -0,0 +1,18 @@
+document.getElementById("login-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const accountNumber =document.getElementById("account-number").value;
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+    if(accountNumber.length === 11){
+        if(convertedPin === 1234){
+            window.location.href = "./main.html"
+        }
+        else{
+            alert("pin thik nai");
+        }
+    }
+    else{
+        alert("need valid account number")
+    }
+    
+})
